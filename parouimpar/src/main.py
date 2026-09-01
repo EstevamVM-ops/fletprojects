dc547aa10a060caf4f6f19fe8b4dd3c80b7405ae
estevam <estevamvigilatomatsuyama@gmail.com>
2026-09-01 09:26:46 -0300

    par ou impar alert

--- parouimpar/src/main.py
+++ parouimpar/src/main.py
@@ -3,19 +3,38 @@ import flet as ft
 
 def main(page: ft.Page):
     # --- eventlistner
+    def colose_dialog(e):
+        page.pop_dialog(),
+        page.update(),
+    
     def on_click_send(e):
         value = input_txt.value
         try:
             int_value= int(value)
+            output_txt = ft.Text(value ='')
             if(int_value % 2 == 0):
-                output_txt.value += (f'{int_value} é par\n')
+                output_txt.value = (f'{int_value} é par')
+                
             else:
-                 output_txt.value += (f'{int_value} é impar\n')
-            print(int_value)
+                output_txt.value = (f'{int_value} é impar')
+            output_col.controls.append(output_txt)
+        
         except ValueError:
-            output_txt.value += f'"{value}" não é um valor inteiro.\n'
-            input_txt.value = ''
+            if(value.strip() == ''):
+                dialog.content.value = 'O campo esta vazio.'
+            else:
+                dialog.content.value = f'"{value}" não é um valor inteiro'    
+            page.show_dialog(dialog)
+        input_txt.value = ''
+        page.update
     # ----Widgets 
+    dialog = ft.AlertDialog(
+        title=ft.Text('Erro'),
+        content=ft.Text('Mensagem erro'),
+        actions=[
+            ft.TextButon('Fechar')
+        ]
+    )
     input_txt = ft. TextField(
         expand=True,
         hint_text='Digite um numero.....')
@@ -23,7 +42,8 @@ def main(page: ft.Page):
     input_btn= ft.IconButton( 
         icon=ft.Icons.SEND,
         on_click = on_click_send)
-     
+
+
     output_txt = ft.Text(value = '')
 
     # ----- Layout
@@ -34,8 +54,9 @@ def main(page: ft.Page):
     output_col = ft.Column(
         expand=True,
         horizontal_aligmnts = ft.CrossAxisAlignment.STRETCH,
-        scroll=ft.ScrollMode.ALWAYS,
-        controls= [output_txt]
+        spacing=0,
+        scroll=ft.ScrollMode.AUTO,
+        controls= []
     )
 
     main_col = ft.Column(
